574ecfc038bc3f8bd5d74a331ae18aa2e4544aa4
jatmdm <jatmdm@gmail.com>
2015-04-11 02:35:55 -0400

    Added Revanent EnemyAI

--- Rampant/Assets/Scripts/AI/EnemyAI.cs
+++ Rampant/Assets/Scripts/AI/EnemyAI.cs
@@ -6,6 +6,8 @@ public class EnemyAI : MonoBehaviour {
 	public Vector2 targetPosition;
 	public Vector2 dir = Vector2.zero;
 
+	public float speed;
+
 	public float followDistance;
 	// Use this for initialization
 	void Start () {
@@ -13,17 +15,21 @@ public class EnemyAI : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		if(Vector2.Distance(GameObject.FindGameObjectWithTag("Player").transform.position, this.gameObject.transform.position) < followDistance){
 			targetPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
 		}
+		else{
+			targetPosition = this.gameObject.transform.position;
+		}
+
 		if(targetPosition == null){
 			dir = Vector2.zero;
 		}
 		else{
 			dir = Vector2.Lerp(dir, this.GetComponent<Agent>().pathing(targetPosition, 1f), Time.fixedDeltaTime*10);
 		}
-		GetComponent<Rigidbody2D> ().MovePosition (GetComponent<Rigidbody2D> ().position + dir * 5 * Time.fixedDeltaTime);
+		GetComponent<Rigidbody2D> ().MovePosition (GetComponent<Rigidbody2D> ().position + dir * speed * Time.fixedDeltaTime);
 
 	}
 }
